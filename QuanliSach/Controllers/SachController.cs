37de5ec01a83efb3dd96e840ce07473ce8bb7dda
Tai Nguyen <anhtai806@outlook.com>
2021-07-16 18:32:05 +0700

    Add project files.

---
+++ QuanliSach/Controllers/SachController.cs
@@ -0,0 +1,73 @@
+﻿using QuanliSach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuanliSach.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+ {
+            new Sach { Id = 1, Title = "Tôi thấy hoa vàng trên cỏ xanh", AuthorName ="Nguyễn Nhật Ánh", Price = 1, Content="Truyện kể về Tuổi thơ..." },
+            new Sach { Id = 2, Title = "Pro ASP.NET MVC5", AuthorName = "Adam Freeman", Content= "The ASP.NET MVC 5 Framework is the latest evolution of Microsoft’sASP.NET web platform.", Price = 3.75M },
+ };
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.Id == id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+        [HttpGet]
+        public List<Sach> GetSachLists()
+        {
+            dbsach1DataContext db = new dbsach1DataContext();
+            return dbsach1.ToList();
+        }
+        [HttpPut]
+        public bool UpdateSach(int id, string Content, string Title, string AuthorName, decimal Price)
+        {
+            try
+            {
+                dbsach1DataContext db = new dbsach1DataContext();
+                Sach sach = dbSaches.FirstOrDefault(p => p.Id == id);
+                if (sach == null) return false;
+                sach.AuthorName = AuthorName;
+                sach.Title = Title;
+                sach.Content = Content;
+                sach.Price = Price;
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        [HttpDelete]
+        public bool DeleteSach(int id)
+        {
+            dbsach1DataContext db = new dbsach1DataContext();
+            //lấy food tồn tại ra
+            Sach sach = db.Saches.FirstOrDefault(x => x.Id == id);
+            if (sach == null) return false;
+            db.Saches.DeleteOnSubmit(sach);
+            db.SubmitChanges();
+            return true;
+        }
+    }
+
+}
+   
---
+++ QuanliSach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanliSach.WebApiApplication" Language="C#" %>
